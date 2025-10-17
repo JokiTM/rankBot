e67db32262f9eebe950b9d1adaa4522dd92603c5
joki <mathisjoelk@outlook.de>
2025-10-17 18:54:13 +0200

    Added logging in initiation method

--- src/main/java/com/kaiser/rankbot/MyListener.java
+++ src/main/java/com/kaiser/rankbot/MyListener.java
@@ -18,118 +18,117 @@ import java.util.Objects;
 
 @Component
 public class MyListener extends ListenerAdapter {
-    private static final Logger logger = LoggerFactory.getLogger(MyListener.class);
+  private static final Logger logger = LoggerFactory.getLogger(MyListener.class);
 
-    @Autowired
-    private RankService rankService;
-    @Autowired
-    private RankRepo repo;
-    @Autowired
-    private JDA jda;
+  @Autowired
+  private RankService rankService;
+  @Autowired
+  private RankRepo repo;
+  @Autowired
+  private JDA jda;
 
+  @PostConstruct
+  public void init() {
+    logger.info("Initiating rankBot");
+    jda.addEventListener(this);
+    logger.info("Added event listener");
+    CommandService.registerCommands(jda);
+    logger.info("Registrated slash Commands");
+    logger.info("Intiated rankBot. Listening for events...");
+  }
 
-    @PostConstruct
-            public void init() {
-        jda.addEventListener(this);
-        CommandService.registerCommands(jda);
+  String helpMessage = """
+      **Verfügbare Befehle:**
+      - `/setuser <Summoner-Name>` - Verlinkt Discord Profil mit League Account Format:[Joki#ANT].
+      - `/help` - Zeigt diese Nachricht an.
+      - `/removeuser` - Entfernt den nutzer aus der Datenbank, sodass der name erneut geändert werden kann.
+      - `/setnickname` - Ändert den nickname des Users.
 
+      """;
+
+  @Override
+  public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+    logger.info("Slash command interaction");
+
+    if (event.getName().equals("setuser")) {
+      setUser(event);
+    } else if (event.getName().equals("removeuser")) {
+      removeUser(event);
+    } else if (event.getName().equals("help")) {
+      event.getChannel().sendMessage(helpMessage).queue();
+    } else if (event.getName().equals("setnickname")) {
+      setNickname(event);
     }
 
+  }
 
-    String helpMessage = """
-        **Verfügbare Befehle:**
-        - `/setuser <Summoner-Name>` - Verlinkt Discord Profil mit League Account Format:[Joki#ANT].
-        - `/help` - Zeigt diese Nachricht an.
-        - `/removeuser` - Entfernt den nutzer aus der Datenbank, sodass der name erneut geändert werden kann.
-        - `/setnickname` - Ändert den nickname des Users.
-    
-        """;
-
-
-
-    @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
-        logger.info("Slash command interaction");
-
-        if(event.getName().equals("setuser")){
-            setUser(event);
-        }else if(event.getName().equals("removeuser")){
-            removeUser(event);
-        }else if(event.getName().equals("help")){
-               event.getChannel().sendMessage(helpMessage).queue();
-        } else if(event.getName().equals("setnickname")){
-            setNickname(event);
-        }
-
+  private void setNickname(SlashCommandInteractionEvent event) {
+    logger.info("Set Nickname");
+    var user = event.getUser();
+    var newName = Objects.requireNonNull(event.getOption("nickname")).getAsString();
+    if (newName.length() > 16) {
+      event.reply("Nickname is too long(Max 16 chars").queue();
+      return;
     }
-
-    private void setNickname(SlashCommandInteractionEvent event) {
-        logger.info("Set Nickname");
-        var user = event.getUser();
-        var newName = Objects.requireNonNull(event.getOption("nickname")).getAsString();
-        if(newName.length() > 16){
-            event.reply("Nickname is too long(Max 16 chars").queue();
-            return;
-        }
-        var userRank = repo.findById(user.getId()).orElse(null);
-        if(userRank == null) {
-            event.reply("Nutzer nicht registriert! Benutzer /setuser, um dich zu registrieren!").queue();
-            return;
-        }
-        repo.updateDiscordName(user.getId(), newName);
-        rankService.modifyNickname(Objects.requireNonNull(event.getGuild()), userRank, newName + " ~ " + userRank.getTier().charAt(0) + " " + userRank.getRank() + " | " + userRank.getLeaguePoints() + "LP");
-        event.reply("Nickname set!").queue();
+    var userRank = repo.findById(user.getId()).orElse(null);
+    if (userRank == null) {
+      event.reply("Nutzer nicht registriert! Benutzer /setuser, um dich zu registrieren!").queue();
+      return;
     }
+    repo.updateDiscordName(user.getId(), newName);
+    rankService.modifyNickname(Objects.requireNonNull(event.getGuild()), userRank, newName + " ~ "
+        + userRank.getTier().charAt(0) + " " + userRank.getRank() + " | " + userRank.getLeaguePoints() + "LP");
+    event.reply("Nickname set!").queue();
+  }
 
-    private void removeUser(SlashCommandInteractionEvent event){
-        logger.info("Remove user");
-        repo.deleteById(event.getUser().getId());
-        event.reply("Deleted user").queue();
+  private void removeUser(SlashCommandInteractionEvent event) {
+    logger.info("Remove user");
+    repo.deleteById(event.getUser().getId());
+    event.reply("Deleted user").queue();
+  }
+
+  private void setUser(SlashCommandInteractionEvent event) {
+    logger.info("setuser");
+
+    try {
+      var user = rankService.setUser(event);
+      logger.info("setuser user retrieved: {}", user);
+      repo.save(user);
+      event.reply(user.getTier() + " " + user.getRank() + " | " + user.getLeaguePoints() + " LP").queue();
+    } catch (Exception e) {
+      logger.info(e.getMessage());
+      if (e.getMessage().equals("riotId is invalid!")) {
+        event.reply("riotId is invalid!").queue();
+      } else if (e.getMessage().equals("Error while fetching puuid from riot API")) {
+        event.reply("Error while fetching puuid from riot API. User was not saved.").queue();
+      }
     }
+  }
 
+  @Scheduled(fixedRate = 15000)
+  private void updateUser() {
 
-    private void setUser(SlashCommandInteractionEvent event){
-        logger.info("setuser");
+    logger.info("Updating users");
+    List<UserRank> userList = repo.findAll();
 
-        try {
-            var user = rankService.setUser(event);
-            logger.info("setuser user retrieved: {}", user);
-            repo.save(user);
-            event.reply(user.getTier() + " " + user.getRank() + " | " + user.getLeaguePoints() + " LP").queue();
-        } catch (Exception e) {
-            logger.info(e.getMessage());
-            if (e.getMessage().equals("riotId is invalid!")) {
-                event.reply("riotId is invalid!").queue();
-            } else if (e.getMessage().equals("Error while fetching puuid from riot API")) {
-                event.reply("Error while fetching puuid from riot API. User was not saved.").queue();
-            }
-        }
+    for (UserRank user : userList) {
+      logger.info("Updating User: {}; id: {}", user.getDiscordName(), user.getDiscordId());
+      var guild = jda.getGuildById(user.getGuildId());
+      if (guild == null) {
+        logger.error("Guild {} not found. Skipping User {}", user.getGuildId(), user.getDiscordId());
+        // repo.deleteById(user.getDiscordId());
+        continue;
+      }
+      try {
+        rankService.updateUser(user);
+        rankService.modifyNickname(guild, user, user.getDiscordName() + " ~ " + user.getTier().charAt(0) + " "
+            + user.getRank() + " | " + user.getLeaguePoints() + "LP");
+
+      } catch (Exception e) {
+        logger.error("Couldn't update user: {}; Error: {}. ", user.getDiscordName(), e.getMessage());
+      }
     }
+    logger.info("Finished updating users\n\n\n\n\n\n");
 
-    @Scheduled(fixedRate = 15000)
-    private void updateUser(){
-
-        logger.info("Updating users");
-        List<UserRank> userList = repo.findAll();
-
-        for (UserRank user : userList) {
-                logger.info("Updating User: {}; id: {}", user.getDiscordName(), user.getDiscordId());
-                var guild = jda.getGuildById(user.getGuildId());
-                if(guild == null) {
-                    logger.error("Guild {} not found. Skipping User {}", user.getGuildId(), user.getDiscordId());
-                    //repo.deleteById(user.getDiscordId());
-                    continue;
-                }
-            try {
-                rankService.updateUser(user);
-                rankService.modifyNickname(guild, user, user.getDiscordName() + " ~ " + user.getTier().charAt(0) + " " + user.getRank() + " | " + user.getLeaguePoints() + "LP");
-
-            }catch (Exception e) {
-                logger.error("Couldn't update user: {}; Error: {}. ",user.getDiscordName(), e.getMessage());
-            }
-        }
-        logger.info("Finished updating users\n\n\n\n\n\n");
-
-
-    }
+  }
 }

84c6eb585663f7bb51d13b5b93df99873e48e0d6
joki <mathisjoelk@outlook.de>
2026-02-08 00:18:12 +0100

    ölsafd

--- src/main/java/com/kaiser/rankbot/service/RankService.java
+++ src/main/java/com/kaiser/rankbot/service/RankService.java
@@ -79,7 +79,7 @@ public class RankService {
 
     if(log) logger.info("Modify nickname from: {} | rank:{} {} | {} LP", user.getDiscordName(), user.getTier(), user.getRank(), user.getLeaguePoints());
 
-    if(tier.equals("U")){
+    if(tier.equals("Unranked")){
       guild.retrieveMemberById(user.getDiscordId()).queue(member -> guild.modifyNickname(member, "Unranked in LoL").queue());
     }else{
     guild.retrieveMemberById(user.getDiscordId()).queue(member -> guild.modifyNickname(member, newNickname).queue());
